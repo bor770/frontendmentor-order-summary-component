709c2d15686e38a2662f25e5bb177a6f03c58c44
Baruch Giverts <bor770@gmail.com>
2023-05-16 16:48:26 +0300

    in middle

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
-import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
+  imports: [CommonModule, MatButtonModule, MatCardModule],
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule],
+  styleUrls: ['./app.component.css'],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'order-summary-component';
